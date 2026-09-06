d4bb6923f111bedf71920850b45e52bbb12c1e4b
Chloé Mingarine <chloemingarine@MacBook-Air-de-Chloe.local>
2026-09-06 18:59:05 +0200

    atoi-base

---
+++ level3/ft_atoi_base.c
@@ -0,0 +1,36 @@
+int	ft_atoi_base(const char *str, int str_base)
+{
+	int	result;
+	int	sign;
+	int	digit;
+
+	result = 0;
+	sign = 1;
+	if (str_base < 2 || str_base > 16)
+		return (0);
+	if (*str == '-')
+	{
+		sign = -1;
+		str++;
+	}
+	while (*str)
+	{
+		if (*str >= '0' && *str <= '9')
+			digit = *str - '0';
+		else if (*str >= 'a' && *str <= 'f')
+			digit = *str - 'a' + 10;
+		else if (*str >= 'A' && *str <= 'F')
+			digit = *str - 'A' + 10;
+		else
+			break;
+		if (digit >= str_base)
+			break;
+		result = result * str_base + digit;
+		str++;
+	}
+	return (result * sign);
+}
+int main(void)
+{
+    
+}
